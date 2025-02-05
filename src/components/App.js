055fc642d997ce2d78982c43f11ed24147dd17ce
Yuvraj Singh <yuvrajisking66@gmail.com>
2025-02-05 19:41:17 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
+import { useState } from "react";
 import React from "react";
-import './../styles/App.css';
 
-const App = () => {
+const PersonalizedGreeting = () => {
+  const [name, setName] = useState("");
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div >
+      <h2>Enter your name:</h2>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        
+      />
+      {name && <h2 >Hello, {name}!</h2>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default PersonalizedGreeting;
